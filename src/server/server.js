99af6818e2832cb010d54d134a7d9480ab8dccfd
Joni Mustaniemi <mustaniemijm@gmail.com>
2024-11-27 17:41:46 +0200

    Adds azure cosmos integration

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const { CosmosClient } = require("@azure/cosmos");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import { CosmosClient } from "@azure/cosmos";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const corsOptions = {
   origin: [
     "http://localhost",
     "capacitor://localhost",
-    "workoutbackend.azurewebsites.net",
+    process.env.VITE_API_URL,
   ],
 };
 
@@ -19,24 +21,47 @@ const client = new CosmosClient({
   endpoint: process.env.COSMOS_URI,
   key: process.env.COSMOS_KEY,
 });
-const database = client.database("DatabaseName");
-const container = database.container("ContainerName");
 
-app.get("/", async (req, res) => {
+const database = client.database(process.env.DATABASE_NAME);
+const container = database.container(process.env.CONTAINER_NAME);
+
+const testConnection = async () => {
   try {
-    const { resources } = await container.items.readAll().fetchAll();
-    res.status(200).json(resources);
+    console.log("Attempting to connect to Cosmos DB...");
+    const { resources: databases } = await client.databases
+      .readAll()
+      .fetchAll();
+    console.log(
+      `Successfully connected to Cosmos DB. Found ${databases.length} databases.`
+    );
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    console.error("Error connecting to Cosmos DB:", err.message);
+    process.exit(1);
   }
-});
+};
+
+const fetchAllItems = async () => {
+  try {
+    console.log("Fetching items from Cosmos DB...");
+    const { resources } = await container.items.readAll().fetchAll();
+
+    console.log("Fetched items from Cosmos DB:", resources);
+  } catch (err) {
+    console.error("Error fetching data from Cosmos DB:", err.message);
+  }
+};
+
+testConnection();
+fetchAllItems();
 
 app.post("/items", async (req, res) => {
   try {
     const newItem = req.body;
+    console.log("Adding new item to Cosmos DB:", newItem);
     const { resource } = await container.items.create(newItem);
     res.status(201).json(resource);
   } catch (err) {
+    console.error("Error adding item to Cosmos DB:", err.message);
     res.status(500).json({ error: err.message });
   }
 });
