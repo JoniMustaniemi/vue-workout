3cb5ba72637dba6ecad81b62b6cdaf35b017c84a
Joni Mustaniemi <mustaniemijm@gmail.com>
2024-11-27 18:27:28 +0200

    Module spesifier alteration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import { createApp } from "vue";
 import App from "./App.vue";
 import { Quasar } from "quasar";
 import router from "./router";
 import "quasar/dist/quasar.css";
 import "@quasar/extras/material-icons/material-icons.css";
 
-const app = createApp(App);
+const app = Vue.createApp(App);
 
 app.use(Quasar);
 app.use(router);
