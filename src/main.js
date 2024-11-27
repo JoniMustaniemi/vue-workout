2ec75a46af91bc70bf92aa572bfc25edf7e609a9
Joni Mustaniemi <mustaniemijm@gmail.com>
2024-11-27 18:31:17 +0200

    configuration improvements

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
+import { createApp } from "vue";
 import App from "./App.vue";
 import { Quasar } from "quasar";
 import router from "./router";
 import "quasar/dist/quasar.css";
 import "@quasar/extras/material-icons/material-icons.css";
 
-const app = Vue.createApp(App);
+const app = createApp(App);
 
 app.use(Quasar);
 app.use(router);
